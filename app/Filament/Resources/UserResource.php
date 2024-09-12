5a0fd40ac3824cf3280d9bf6a2837a64e6d72e5f
Lucas <lucashenriqueslima@outlook.com>
2024-09-12 13:08:59 -0300

    feat: added user resource

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nome')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')
+                    ->label('E-mail')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('progress_count')
+                    ->counts([
+                        'progress' => fn(Builder $query) => $query->where('is_completed', true),
+                    ])
+                    ->label('Aulas Assistidas')
+                    ->sortable(),
+                TextColumn::make('last_access_log.last')
+                    ->label('Último acesso')
+                    ->getStateUsing(function ($record) {
+                        return optional($record->accessLogs->first())->created_at ?? '2024-01-01 00:00:00';
+                    })
+                    ->searchable()
+                    ->sortable()
+                    ->since(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+        ];
+    }
+}
