38a07bcb376a375ab4a7b2345cab7a79d47ca70e
Lucas Henrique <lucashenriqueslima@outlook.com>
2024-12-27 13:50:28 -0300

    fix/addead-can-access-filament-funticon

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,18 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     /**
      * The attributes that are mass assignable.
