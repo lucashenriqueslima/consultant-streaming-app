61ae7ebd346733d3d56791df45c778c805a1db88
Lucas Henrique <lucashenriqueslima@outlook.com>
2024-12-27 11:48:24 -0300

    fix/typo-candidate-register

--- app/Livewire/Candidate/Register.php
+++ app/Livewire/Candidate/Register.php
@@ -202,8 +202,10 @@ class Register extends Component implements HasForms, HasActions
             $this->data = $this->form->getState();
             $databaseConnection = Association::from($this->data['association'])->getDatabaseConnection();
 
-            if (Candidate::where('cpf', $this->data['cpf'])->exists() ||
-                ConsultantIleva::on($databaseConnection)->where('cpf', $this->data['cpf'])->exists()) {
+            if (
+                Candidate::where('cpf', $this->data['cpf'])->exists() ||
+                ConsultantIleva::on($databaseConnection)->where('cpf', $this->data['cpf'])->exists()
+            ) {
                 Log::info(__FILE__ . " - CPF ja cadastrado", ['cpf' => $this->data['cpf'], 'status' => ConsultantIleva::on($databaseConnection)->where('cpf', $this->data['cpf'])->exists()]);
                 $this->showCpfAlreadyRegisteredNotification();
                 return;
@@ -230,18 +232,18 @@ class Register extends Component implements HasForms, HasActions
                 $this->dispatch('dispatch-consult-sheet');
 
                 Notification::make()
-                ->title('Seu cadastro está aguardando aprovação. Verifique seu e-mail, em breve você receberá uma mensagem de confirmação.')
-                ->warning()
-                ->persistent()
-                ->body('Clique aqui para acessar sua caixa de email.')
-                ->actions([
-                    Action::make('redirect-to-email')
-                        ->label('Caixa de Email')
-                        ->link()
-                        ->color('primary')
-                        ->url('https://gmail.com')
-                ])
-                ->send();
+                    ->title('Seu cadastro está aguardando aprovação. Verifique seu e-mail, em breve você receberá uma mensagem de confirmação.')
+                    ->warning()
+                    ->persistent()
+                    ->body('Clique aqui para acessar sua caixa de email.')
+                    ->actions([
+                        Action::make('redirect-to-email')
+                            ->label('Caixa de Email')
+                            ->link()
+                            ->color('primary')
+                            ->url('https://gmail.com')
+                    ])
+                    ->send();
                 return;
             }
 
@@ -255,7 +257,7 @@ class Register extends Component implements HasForms, HasActions
                         ->label('Acessar Plataforma')
                         ->button()
                         ->color('success')
-                        ->dispatch('redirect-to-dashboard', [$this->candiate->id])
+                        ->dispatch('redirect-to-dashboard', [$this->candidate->id])
 
                 ])
                 ->send();
